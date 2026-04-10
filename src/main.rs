fdf62fa57363cafc6030e6258296886b5901b4a7
Yonatan-Ethiopia <yonatanethiopia1@gmail.com>
2026-04-11 02:39:19 +0300

    First working version

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::fs::File;
 use std::io::{BufWriter, Read, Write};
 use std::process::{Command, Stdio};
+use std::env;
+use std::sync::{Arc, Mutex};
+use ringbuf::{traits::*, HeapRb};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
 fn get_url(url :&str)->String{
     let stream_url = Command::new("yt-dlp")
@@ -16,8 +20,13 @@ fn get_url(url :&str)->String{
     return out_stream
 }
 
-fn get_audio( stream_url: String){
-    let mut buffer = [0u8; 1024];
+fn get_audio( stream_url: String)-> Result<(), Box<dyn std::error::Error>>{
+    let host = cpal::default_host();
+    let device = host.default_output_device().expect("Couldnt find any sound devices!");
+    let config = device.default_output_config()?;
+    
+    let buffer = HeapRb::<u8>::new(32768);
+    let (mut prod, mut cons) = buffer.split();
     let file = File::create("output.raw").expect("Failed to create file");
     let mut writer = BufWriter::new(file);
     let mut ffmpeg_cmd = Command::new("ffmpeg")
@@ -29,33 +38,88 @@ fn get_audio( stream_url: String){
     "-f", 
     "s16le", 
     "-ac", "2", 
-    "-ar", "44100", 
+    "-ar", "44100",
     "-"
 ])                      // Output to stdout
         .stdout(Stdio::piped())
         .spawn()
         .expect("ffmpeg failed");
-    if let Some(mut stdout) = ffmpeg_cmd.stdout.take(){
-        let mut total_bytes = 0;
-        while let Ok(n) = stdout.read(&mut buffer) {
-            if n == 0 { break; } 
-
-            writer.write_all(&buffer[..n]).expect("Failed to write to file");
-            
-            total_bytes += n;
-            // Update the terminal with the current file size
-            print!("\rSaved: {:.2} MB", total_bytes as f64 / 1_000_000.0);
-            std::io::stdout().flush().unwrap();
-        }
-        writer.flush().expect("Final flush failed");
-        println!("\nDone! Saved {} bytes to {}", total_bytes, "outputffm.raw");
-        }
-           print!("getting data...\n"); 
     
+    std::thread::spawn(move || {
+        if let Some(mut stdout) =  ffmpeg_cmd.stdout.take(){
+            //loop{
+                //match prod.read_from(&mut stdout, None){
+                    //Some(Ok(0))=>break,
+                    //Some(Ok(_))=>continue,
+                    //Some(Err(e)) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                    //// Buffer is full; wait a bit for the consumer to catch up
+                    //std::thread::sleep(std::time::Duration::from_millis(10));
+                //}
+                //Some(Err(_)) => break,
+                //None=>std::thread::sleep(std::time::Duration::from_millis(10)),
+                //}
+            //}
+        loop{
+            if let Some(Ok(bytes)) = prod.read_from(&mut stdout, None){
+                if bytes == 0 { break; }
+            }
+        }
+        }
+    });
+    
+    let stream = device.build_output_stream(
+        &config.into(),
+        move | data: &mut [f32], _:&cpal::OutputCallbackInfo | {
+            let mut byte_buffer = [0u8; 2];
+            for i in data.iter_mut(){
+                
+                if cons.pop_slice(&mut byte_buffer) == 2{
+                    let sample_i16 = i16::from_le_bytes(byte_buffer);
+                    *i = sample_i16 as f32 / 32768.0;
+                }else {
+                    *i = 0.0;
+                }
+            }
+        },
+        |err| eprintln!("An Error occured on stream {}", err), None
+    )?;
+    //if let Some(mut stdout) = ffmpeg_cmd.stdout.take(){
+        //let mut total_bytes = 0;
+        ////while let Ok(n) = stdout.read(&mut buffer) {
+            ////if n == 0 { break; } 
+
+            ////prod.push()expect("Failed to write to file");
+            
+            ////total_bytes += n;
+            ////// Update the terminal with the current file size
+            ////print!("\rSaved: {:.2} MB", total_bytes as f64 / 1_000_000.0);
+            ////std::io::stdout().flush().unwrap();
+        ////}
+        ////
+        //std::thread::spawn(move || {
+			//if let Ok(bytes) = prod.read_from(&mut stdout, None){
+				//if bytes == 0 {break;}
+			//}
+		//})
+        //println!("\nDone! Saved {} bytes to {}", total_bytes, "outputffm.raw");
+        //}
+        print!("getting data...\n");
+        stream.play()?;
+        loop {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+        Ok(())
 }
 
 fn main() {
-    let stream_url = get_url("https://youtu.be/uDl_oBmBIkY?si=eEuMGcL3ZBl83WGl");
-    get_audio(stream_url);
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() > 1 {
+        let stream_url = get_url(&args[1]);
+        get_audio(stream_url);    
+    }
+    else{
+        print!("Please put a proper argument!");
+    }
 }
 
